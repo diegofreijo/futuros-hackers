ccfc58c70d5af2cc937d1b03d0520dad215430d2
Diego <giga.freijo@gmail.com>
2023-10-24 18:58:57 +0000

    finished client side validation

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/idor")
-@app.route("/idor/<int:id>")
-def idor(id:int = None):
-    match id:
+@app.route("/idor", strict_slashes=False)
+@app.route("/idor/<int:cat>")
+def idor(cat:int = None) -> str:
+    match cat:
         case 0:
             image = "/ihfoabsdibiblsdidlbqeirugrq.jpg"
         case 1 | 2 | 3:
-            image = f"{id}.jpg"
+            image = f"{cat}.jpg"
         case _:
             image = None
-            
-    return render_template("idor.html", image=image)
+    return render_template("idor.html", image=image, cat=cat)
+
+
+@app.route("/csv", methods=(['GET']))
+def csv_get():
+    return render_template("csv.html", saldo=100)
+
+@app.route("/csv", methods=(['POST']))
+def csv_post():
+    importe = int(request.form['importe'])
+    return render_template("csv.html", saldo=(100-importe))
