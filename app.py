c77f198c2df13382110e13406eeb134434e1cbf8
Diego <giga.freijo@gmail.com>
2023-10-25 14:56:46 +0000

    finished cookie forgery

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, request
+import base64
+from flask import Flask, make_response, request
 from flask import render_template
 
 app = Flask(__name__)
 
+
 @app.route("/idor", strict_slashes=False)
 @app.route("/idor/<int:cat>")
-def idor(cat:int = None) -> str:
+def idor(cat:int = None):
     match cat:
         case 0:
             image = "/ihfoabsdibiblsdidlbqeirugrq.jpg"
@@ -16,11 +18,31 @@ def idor(cat:int = None) -> str:
     return render_template("idor.html", image=image, cat=cat)
 
 
-@app.route("/csv", methods=(['GET']))
-def csv_get():
-    return render_template("csv.html", saldo=100)
+@app.route("/csv", methods=(['GET', 'POST']))
+def csv():
+    saldo = 100
+    if request.method == 'POST':
+        importe = int(request.form['importe'])
+        saldo -= importe
+    return render_template("csv.html", saldo=saldo)
 
-@app.route("/csv", methods=(['POST']))
-def csv_post():
-    importe = int(request.form['importe'])
-    return render_template("csv.html", saldo=(100-importe))
+
+@app.route("/cf", methods=(['GET', 'POST']))
+def cf():
+    if request.method == 'POST':
+        nombre = "pepe"
+        resp = make_response(render_template('cf.html', nombre=nombre, logueado=True))
+
+        nombre64 = base64.b64encode(nombre.encode())
+        resp.set_cookie('nombre', nombre64.decode())
+
+        return resp
+    else:
+        if 'nombre' in request.cookies:
+            nombre64 = request.cookies.get('nombre')
+            print(nombre64)
+            nombre = base64.b64decode(nombre64).decode()
+        else:
+            nombre = None
+        logueado = nombre is not None
+        return render_template("cf.html", nombre=nombre, logueado=logueado)
